c236e6759829d0cdfbcd5bdfabbae97c8e875b09
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2015-07-27 15:13:21 +0100

    And this

--- lib/Dist/Zilla/PluginBundle/Author/ALTREUS.pm
+++ lib/Dist/Zilla/PluginBundle/Author/ALTREUS.pm
@@ -5,9 +5,30 @@ package Dist::Zilla::PluginBundle::Author::ALTREUS;
 use Moose;
 extends 'Dist::Zilla::PluginBundle::Author::DBOOK';
 
+our $VERSION = '0.001';
+
 before 'configure' => sub {
     my $self = shift;
     $self->{payload}->{github_user} //= 'Altreus';
+
+    $self->add_plugins([Prereqs => { -phase => 'develop', 'Dist::Zilla::PluginBundle::Author::ALTREUS' => $VERSION }]);
 };
 
 1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Dist::Zilla::PluginBundle::Author::ALTREUS - BeLike::ALTREUS, who is exactly
+like DBOOK (except less obsessed with Mojolicious)
+
+=head1 DESCRIPTION
+
+This is exactly DBOOK's bundle except I've overridden the default github username.
+
+=head1 SEE ALSO
+
+L<Dist::Zilla::PluginBundle::Author::DBOOK>
